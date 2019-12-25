51b715d2f7f4c6133dcd4260397ef2946f62b3b2
Jin Wu <jinn.wuu@gmail.com>
2019-12-25 15:05:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,12 +3,12 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
   hash = {:name => "Adam"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {:name => "Grace Hopper"}
 end
 
 def id_generator
